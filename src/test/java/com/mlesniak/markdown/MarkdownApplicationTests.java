3b6c60d6f4e6bc896f6a35dba3aa216f0c6e3ee8
Michael Lesniak <mail@mlesniak.com>
2020-04-07 21:05:11 +0200

    Initial commit

---
+++ src/test/java/com/mlesniak/markdown/MarkdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlesniak.markdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
