c151c16ac768651194409db422f271ed600bfa2a
Michael Lesniak <mail@mlesniak.com>
2020-04-09 06:18:25 +0200

    Render HTML from markdown

--- src/main/java/com/mlesniak/markdown/WebController.java
+++ src/main/java/com/mlesniak/markdown/WebController.java
@@ -6,6 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,17 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-    public class WebController {
+public class WebController {
     Logger LOG = LoggerFactory.getLogger(WebController.class);
 
     @ResponseBody
-    @RequestMapping({"/{name}", "/"})
-    public byte[] requestMarkdown(@PathVariable(name="name", required = false) Optional<String> name) throws IOException {
+    @RequestMapping({ "/{name}", "/" })
+    public byte[] requestMarkdown(@PathVariable(name = "name", required = false) Optional<String> name)
+            throws IOException {
         LOG.info("Markdown file '{}' requested", name);
 
+        // Read content from markdown file.
         var filename = convertFilename(name);
         var content = Files.readString(Path.of(filename));
-        return content.getBytes(Charset.defaultCharset());
+
+        // Parse to markdown.
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(content);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        String output = renderer.render(document);
+
+        return output.getBytes(Charset.defaultCharset());
     }
 
     String convertFilename(Optional<String> name) {
