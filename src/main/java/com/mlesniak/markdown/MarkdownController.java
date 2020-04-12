e276fbd2c040861ecc80a164d14f2d8a40508883
Michael Lesniak <mail@mlesniak.com>
2020-04-12 17:09:23 +0200

    Split controller into multiple files

--- src/main/java/com/mlesniak/markdown/MarkdownController.java
+++ src/main/java/com/mlesniak/markdown/MarkdownController.java
@@ -20,26 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class WebController {
-    Logger LOG = LoggerFactory.getLogger(WebController.class);
-
-    /**
-     * Handler to serve all static files and the favicon.
-     *
-     * @param name requested name inside the static/ directory.
-     * @return content
-     * @throws IOException
-     */
-    @ResponseBody
-    @RequestMapping({ "/static/{name}", "favicon.ico" })
-    public ResponseEntity<byte[]> requestStaticfile(@PathVariable(name = "name", required = false) String name) throws IOException {
-        try {
-            return ResponseEntity.ok(Files.readAllBytes(Path.of("static/" + name)));
-        } catch (NoSuchFileException e) {
-            LOG.info("Requested static file {} not found", name);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
+public class MarkdownController {
+    Logger LOG = LoggerFactory.getLogger(MarkdownController.class);
 
     /**
      * Handler for all markdown files.
---
+++ src/main/java/com/mlesniak/markdown/StaticController.java
@@ -0,0 +1,43 @@
+package com.mlesniak.markdown;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StaticController {
+    Logger LOG = LoggerFactory.getLogger(StaticController.class);
+
+    /**
+     * Handler to serve all static files and the favicon.
+     *
+     * @param name requested name inside the static/ directory.
+     * @return content
+     * @throws IOException
+     */
+    @ResponseBody
+    @RequestMapping({ "/static/{name}", "favicon.ico" })
+    public ResponseEntity<byte[]> requestStaticfile(@PathVariable(name = "name", required = false) String name) throws IOException {
+        try {
+            return ResponseEntity.ok(Files.readAllBytes(Path.of("static/" + name)));
+        } catch (NoSuchFileException e) {
+            LOG.info("Requested static file {} not found", name);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
